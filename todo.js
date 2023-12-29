4347ed0a07620ad1aa952154935c7cc16936078f
Akif <Akif@DESKTOP-T25E8CU>
2023-12-30 00:57:35 +0300

    Proje Tamamlandı

--- todo.js
+++ todo.js
@@ -1,18 +1,58 @@
-var todoInput= document.getElementById("todo");
-var form=document.querySelector("#todo-form");
-var firstCard = document.querySelectorAll(".card-body")[0];
-var secondCard = document.querySelectorAll(".card-body")[1];
-var listGroup = document.querySelector(".list-group");
+let todoInput= document.getElementById("todo");
+let form=document.querySelector("#todo-form");
+let firstCard = document.querySelectorAll(".card-body")[0];
+let secondCard = document.querySelectorAll(".card-body")[1];
+let listGroup = document.querySelector(".list-group");
+let filterInput= document.getElementById("filter");
+let clearButton = document.getElementById("clear-todos");
 
 form.addEventListener("submit",formSubmit)
 document.addEventListener("DOMContentLoaded",loadAllTodosToUI);
 secondCard.addEventListener("click",deleteTodo);
+filterInput.addEventListener("keyup",filterTodo);
+clearButton.addEventListener("click",deleteAll);
+
+function deleteAll(e){
+    if (confirm("Tümünü silmek istiyor musunuz ? ")) {
+        while(listGroup.firstElementChild!=null){
+            listGroup.removeChild(listGroup.firstElementChild);
+        }
+        localStorage.removeItem("todos")
+    }
+    
+}
+
+function filterTodo(e){
+    let value = e.target.value.toLowerCase();
+    let list=document.querySelectorAll(".list-group-item");
+    list.forEach((item,index)=>{
+        let text = item.textContent.toLowerCase();
+        if (text.indexOf(value)=== -1) {
+           item.setAttribute("style","display : none !important")
+        }
+        else{
+            item.setAttribute("style","display : block")
+        }
+    })
+}
 
 function deleteTodo(e){
     if (e.target.className === "fa fa-remove") {
         e.target.parentElement.parentElement.remove();
+        deleteFromLocalStorage(e.target.parentElement.parentElement.textContent);
         showAlert("success","todo başarıyla siilindi");
     }
+}
+
+function deleteFromLocalStorage(deleteTodo){
+    let todos = checkLocalStorage();
+    todos.forEach((item,index)=>{
+        if (item===deleteTodo) {
+            todos.splice(index,1);
+        }
+    });
+    localStorage.setItem("todos",JSON.stringify(todos));
+
 }
 
 function loadAllTodosToUI(){
